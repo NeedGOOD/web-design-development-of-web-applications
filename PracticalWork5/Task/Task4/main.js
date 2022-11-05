44af6506a62293f0c941e49794aa9eb29c5974e8
_Need <vladyakusevich@gmail.com>
2022-11-05 13:53:31 +0200

    new project

---
+++ PracticalWork5/Task/Task4/main.js
@@ -0,0 +1,7 @@
+function LastDay() {
+    let elem = document.getElementById('fourth-block');
+    let date = new Date(
+        Number(document.getElementById('infoYears').value),
+        Number(document.getElementById('infoMonth').value), 0);
+    elem.innerHTML = `Date: ${date.getDate()}`;
+}
