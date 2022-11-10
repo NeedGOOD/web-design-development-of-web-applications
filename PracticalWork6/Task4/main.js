9dbaba6f6c342b7361db7062b065098cdf4dab99
_Need <vladyakusevich@gmail.com>
2022-11-10 21:56:07 +0200

    upd: finall project

--- PracticalWork6/Task4/main.js
+++ PracticalWork6/Task4/main.js
@@ -1,19 +1,10 @@
-function WordStorting() {
-    let elem = document.getElementById('block');
-    let words = document.getElementById('words').value;
+function WordSorting() {
+    let words = prompt();
+    let string = words.split(' ');
 
-    let string = '';
-    let name = '';
-
-    for (let i = 0; i < words.length; ++i) {
-        if (words[i] == ' ') {
-            name = name + string + ' ';
-            string = ''
-        } else { string += words.charAt(i); }
-    }
-
-    for (let i = 0; i < name.length; ++i) {
-        name.sort();
-    }
-    elem.innerHTML = `${name}`;
+    string.sort();
+    
+    alert(string);
 }
+
+WordSorting();
