c33ab56e2d690080b19782e9d040b0dbdeafbe68
_Need <vladyakusevich@gmail.com>
2022-11-10 20:39:30 +0200

    upd: finall project

--- PracticalWork6/Task3/main.js
+++ PracticalWork6/Task3/main.js
@@ -1,43 +1,30 @@
 function NumberOperatorNumber() {
-    let elem = document.getElementById('block');
-    let string = document.getElementById('number-operator-number').value;
+    const string = prompt();
 
-    let strNumber = '';
     let a = 0;
-    let b = 0;
+    let operator = '';
+    let strNumber = '';
 
     for (let i = 0; i < string.length; ++i) {
-        if (string[i] >= '0' && string[i] <= '9') { 
-            strNumber += string.charAt(i);
-            document.write(strNumber[i] + '<br>');
-        }
-        else if (string[i] == '+' || string[i] == '-' ||
-                 string[i] == '*' || string[i] == '/') {
+        if (string.charAt(i) >= '0' && string.charAt(i) <= '9') { strNumber += string.charAt(i); }
+        else if (string.charAt(i) == '+' || string.charAt(i) == '-' ||
+                 string.charAt(i) == '*' || string.charAt(i) == '/') {
+            operator = string.charAt(i);
             a = Number(strNumber);
-            strNumber = '';
+            strNumber = "";
         }
     }
-    b = Number(strNumber);
-    document.write(b);
+    const b = Number(strNumber);
 
+    let result = 0;
 
-    function Sum(a, b) { return a + b; }
-    function Difference(a, b) { return a - b; }
-    function Product(a, b) { return a * b; }
-    function Fraction(a, b) { return a / b; }
-
-    for (let i = 0; i < string.length; ++i) {
-        if (string[i] == '+') { 
-            document.write('+');
-            elem.innerHTML = `${Sum(a, b)}`; }
-        else if (string[i] == '-') { 
-            document.write('-');
-            elem.innerHTML = `${Difference(a, b)}`; }
-        else if (string[i] == '*') { 
-            document.write('*');
-            elem.innerHTML = `${Product(a, b)}`; }
-        else if (string[i] == '/') { 
-            document.write('/');
-            elem.innerHTML = `${Fraction(a, b)}`; }
+    switch (operator) {
+        case "+": result = a + b; break;
+        case "-": result = a - b; break;
+        case "*": result = a * b; break;
+        case "/": result = a / b; break;
     }
+    alert(`${a} ${operator} ${b} = ${result}`)
 }
+
+NumberOperatorNumber();
